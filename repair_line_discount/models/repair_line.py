bd860f68af57d48b5420fed19f9d06afe249a4c3
jesus01x <xzan12@outlook.com>
2021-04-27 16:55:32 -0500

    [FIX] repair_line: the code of function create_invoices was combine to old code of file repair_line.py inside of qualmex module. (#1)
    Like to summery this code add function to can add discount to lines of repair order and this discounts was send it to invoices created.
    When the user press button Create Invoice.

--- repair_line_discount/models/repair_line.py
+++ repair_line_discount/models/repair_line.py
@@ -1,102 +1,181 @@
 # Copyright 2020, Jarsa
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+
+import logging
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+from collections import defaultdict
+from odoo.tools import float_is_zero
 
+_logger = logging.getLogger(__name__)
 
 class RepairOrder(models.Model):
     _inherit = "repair.order"
 
     def _create_invoices(self, group=False):
-        res = super()._create_invoices(group)
-        self._add_lines_with_discount()
-        return res
+        grouped_invoices_vals = {}
+        repairs = self.filtered(
+            lambda repair: repair.state not in ('draft', 'cancel')
+            and not repair.invoice_id
+            and repair.invoice_method != 'none')
+        for repair in repairs:
+            repair = repair.with_company(repair.company_id)
+            partner_invoice = repair.partner_invoice_id or repair.partner_id
+            if not partner_invoice:
+                raise UserError(_('You have to select an invoice address in the repair form.'))
 
-    def _add_lines_with_discount(self):
-        currency = self.pricelist_id.currency_id
-        company = self.env.company
-        invoice = self.invoice_id
-        invoice.mapped("line_ids").unlink()
-        invoice.mapped("invoice_line_ids").unlink()
-        invoice_vals = []
-        for operation in self.operations:
-            account = operation.product_id.product_tmpl_id._get_product_accounts()[
-                "income"
-            ]
-            if not account:
-                raise ValidationError(
-                    _('No account defined for product "%s".', operation.product_id.name)
-                )
+            narration = repair.quotation_notes
+            currency = repair.pricelist_id.currency_id
+            company = repair.env.company
 
-            invoice_line_vals = {
-                "name": operation.name,
-                "account_id": account.id,
-                "quantity": operation.product_uom_qty,
-                "tax_ids": [(6, 0, operation.tax_id.ids)],
-                "product_uom_id": operation.product_uom.id,
-                "price_unit": operation.price_unit,
-                "product_id": operation.product_id.id,
-                "discount": operation.discount,
-                "repair_line_ids": [(4, operation.id)],
-            }
-            if currency == company.currency_id:
-                balance = -(operation.product_uom_qty * operation.price_unit)
-                invoice_line_vals.update(
-                    {
-                        "debit": balance > 0.0 and balance or 0.0,
-                        "credit": balance < 0.0 and -balance or 0.0,
-                    }
-                )
+            journal = repair.env['account.move'].with_context(move_type='out_invoice')._get_default_journal()
+            if not journal:
+                raise UserError(
+                    _('Please define an accounting sales journal for the company %s (%s).')
+                    % (company.name, company.id))
+
+            if (partner_invoice.id, currency.id) not in grouped_invoices_vals:
+                grouped_invoices_vals[(partner_invoice.id, currency.id, company.id)] = []
+            current_invoices_list = grouped_invoices_vals[
+                (partner_invoice.id, currency.id, company.id)]
+
+            if not group or len(current_invoices_list) == 0:
+                fpos = self.env['account.fiscal.position'].get_fiscal_position(
+                    partner_invoice.id, delivery_id=repair.address_id.id)
+                invoice_vals = {
+                    'move_type': 'out_invoice',
+                    'partner_id': partner_invoice.id,
+                    'partner_shipping_id': repair.address_id.id,
+                    'currency_id': currency.id,
+                    'narration': narration,
+                    # 'line_ids': [],
+                    'invoice_origin': repair.name,
+                    'repair_ids': [(4, repair.id)],
+                    'invoice_line_ids': [],
+                    'fiscal_position_id': fpos.id
+                }
+                if partner_invoice.property_payment_term_id:
+                    invoice_vals['invoice_payment_term_id'] = partner_invoice.property_payment_term_id.id
+                current_invoices_list.append(invoice_vals)
             else:
-                amount_currency = -(operation.product_uom_qty * operation.price_unit)
-                balance = currency._convert(
-                    amount_currency, company.currency_id, company, fields.Date.today()
-                )
-                invoice_line_vals.update(
-                    {
-                        "amount_currency": amount_currency,
-                        "debit": balance > 0.0 and balance or 0.0,
-                        "credit": balance < 0.0 and -balance or 0.0,
-                        "currency_id": currency.id,
-                    }
-                )
-            invoice_vals.append((0, 0, invoice_line_vals))
-        for fee in self.fees_lines:
-            invoice_line_vals = {
-                "name": fee.name,
-                "account_id": account.id,
-                "quantity": fee.product_uom_qty,
-                "tax_ids": [(6, 0, fee.tax_id.ids)],
-                "product_uom_id": fee.product_uom.id,
-                "price_unit": fee.price_unit,
-                "discount": fee.discount,
-                "product_id": fee.product_id.id,
-                "repair_fee_ids": [(4, fee.id)],
-            }
-            if currency == company.currency_id:
-                balance = -(fee.product_uom_qty * fee.price_unit)
-                invoice_line_vals.update(
-                    {
-                        "debit": balance > 0.0 and balance or 0.0,
-                        "credit": balance < 0.0 and -balance or 0.0,
-                    }
-                )
-            else:
-                amount_currency = -(fee.product_uom_qty * fee.price_unit)
-                balance = currency._convert(
-                    amount_currency, company.currency_id, company, fields.Date.today()
-                )
-                invoice_line_vals.update(
-                    {
-                        "amount_currency": amount_currency,
-                        "debit": balance > 0.0 and balance or 0.0,
-                        "credit": balance < 0.0 and -balance or 0.0,
-                        "currency_id": currency.id,
-                    }
-                )
-            invoice_vals.append((0, 0, invoice_line_vals))
-        invoice.write({"invoice_line_ids": invoice_vals})
+                # if group == True: concatenate invoices by partner and currency
+                invoice_vals = current_invoices_list[0]
+                invoice_vals['invoice_origin'] += ', ' + repair.name
+                invoice_vals['repair_ids'].append((4, repair.id))
+                if not invoice_vals['narration']:
+                    invoice_vals['narration'] = narration
+                else:
+                    invoice_vals['narration'] += '\n' + narration
+
+            # Create invoice lines from operations.
+            for operation in repair.operations.filtered(lambda op: op.type == 'add'):
+                if group:
+                    name = repair.name + '-' + operation.name
+                else:
+                    name = operation.name
+
+                account = operation.product_id.product_tmpl_id._get_product_accounts()['income']
+                if not account:
+                    raise UserError(_('No account defined for product "%s".', operation.product_id.name))
+
+                invoice_line_vals = {
+                    'name': name,
+                    'account_id': account.id,
+                    'quantity': operation.product_uom_qty,
+                    'tax_ids': [(6, 0, operation.tax_id.ids)],
+                    'product_uom_id': operation.product_uom.id,
+                    'price_unit': operation.price_unit,
+                    'product_id': operation.product_id.id,
+                    "discount": operation.discount,
+                    'repair_line_ids': [(4, operation.id)],
+                }
+
+                if currency == company.currency_id:
+                    balance = -(operation.product_uom_qty * operation.price_unit)
+                    invoice_line_vals.update({
+                        'debit': balance > 0.0 and balance or 0.0,
+                        'credit': balance < 0.0 and -balance or 0.0,
+                    })
+                else:
+                    amount_currency = -(operation.product_uom_qty * operation.price_unit)
+                    balance = currency._convert(amount_currency, company.currency_id, company, fields.Date.today())
+                    invoice_line_vals.update({
+                        'amount_currency': amount_currency,
+                        'debit': balance > 0.0 and balance or 0.0,
+                        'credit': balance < 0.0 and -balance or 0.0,
+                        'currency_id': currency.id,
+                    })
+                invoice_vals['invoice_line_ids'].append((0, 0, invoice_line_vals))
+
+            # Create invoice lines from fees.
+            for fee in repair.fees_lines:
+                if group:
+                    name = repair.name + '-' + fee.name
+                else:
+                    name = fee.name
+
+                if not fee.product_id:
+                    raise UserError(_('No product defined on fees.'))
+
+                account = fee.product_id.product_tmpl_id._get_product_accounts()['income']
+                if not account:
+                    raise UserError(_('No account defined for product "%s".', fee.product_id.name))
+
+                invoice_line_vals = {
+                    'name': name,
+                    'account_id': account.id,
+                    'quantity': fee.product_uom_qty,
+                    'tax_ids': [(6, 0, fee.tax_id.ids)],
+                    'product_uom_id': fee.product_uom.id,
+                    'price_unit': fee.price_unit,
+                    "discount": fee.discount,
+                    'product_id': fee.product_id.id,
+                    'repair_fee_ids': [(4, fee.id)],
+                }
+
+                if currency == company.currency_id:
+                    balance = -(fee.product_uom_qty * fee.price_unit)
+                    invoice_line_vals.update({
+                        'debit': balance > 0.0 and balance or 0.0,
+                        'credit': balance < 0.0 and -balance or 0.0,
+                    })
+                else:
+                    amount_currency = -(fee.product_uom_qty * fee.price_unit)
+                    balance = currency._convert(amount_currency, company.currency_id, company,
+                                                fields.Date.today())
+                    invoice_line_vals.update({
+                        'amount_currency': amount_currency,
+                        'debit': balance > 0.0 and balance or 0.0,
+                        'credit': balance < 0.0 and -balance or 0.0,
+                        'currency_id': currency.id,
+                    })
+                invoice_vals['invoice_line_ids'].append((0, 0, invoice_line_vals))
+
+        # Create invoices.
+        invoices_vals_list_per_company = defaultdict(list)
+        for (partner_invoice_id, currency_id, company_id), invoices in grouped_invoices_vals.items():
+            for invoice in invoices:
+                _logger.info("%s %s %s" % (partner_invoice_id, self._name, 1))
+                _logger.info("%s %s %s" % (currency_id, self._name, 1))
+                invoices_vals_list_per_company[company_id].append(invoice)
+
+        for company_id, invoices_vals_list in invoices_vals_list_per_company.items():
+            # VFE TODO remove the default_company_id ctxt key ?
+            # Account fallbacks on self.env.company, which is correct with with_company
+            self.env['account.move'].with_company(
+                company_id).with_context(
+                default_company_id=company_id,
+                default_move_type='out_invoice').create(
+                    invoices_vals_list)
+        repairs.write({'invoiced': True})
+        repairs.mapped('operations').filtered(
+            lambda op: op.type == 'add').write(
+                {'invoiced': True})
+        repairs.mapped('fees_lines').write({'invoiced': True})
+
+        return dict(
+            (repair.id, repair.invoice_id.id) for repair in repairs)
 
 
 class RepairLine(models.Model):
